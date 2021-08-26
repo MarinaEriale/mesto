c85b4800d54d5fe359193c1fc0684bb0e5ad636f
Marina Ivanova <marina.u.ivanova@gmail.com>
2021-08-26 18:22:49 +0300

    fixes after first review

--- script.js
+++ script.js
@@ -2,14 +2,6 @@ let openPopupButton = document.querySelector('.profile__open-popup');
 let popup = document.querySelector('.popup');
 let closePopupButton = document.querySelector('.popup__close-popup');
 
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
-}
-
-openPopupButton.addEventListener('click', togglePopup);
-
-closePopupButton.addEventListener('click', togglePopup);
-
 let formElement = document.querySelector('.popup__container');
 
 let nameInput = formElement.querySelector('.popup__text_type_name');
@@ -18,14 +10,24 @@ let jobInput = formElement.querySelector('.popup__text_type_profession');
 let nameProfile = document.querySelector('.profile__name');
 let jobProfile = document.querySelector('.profile__profession');
 
-nameInput.value = nameProfile.textContent;
-jobInput.value = jobProfile.textContent;
+function togglePopup() {
+  popup.classList.toggle('popup_opened');
+}
 
 function formSubmitHandler (event) {
-    event.preventDefault();
-    nameProfile.textContent = nameInput.value;
-    jobProfile.textContent = jobInput.value;
-    togglePopup();
+  event.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  togglePopup();
 }
+
+function editProfile () {
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+}
+
+openPopupButton.addEventListener('click', togglePopup);
+openPopupButton.addEventListener('click', editProfile);
+closePopupButton.addEventListener('click', togglePopup);
 
 formElement.addEventListener('submit', formSubmitHandler);
